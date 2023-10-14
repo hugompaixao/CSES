3e410a04fa89b7acf1572c191b24eb7af278a9cc
hugompaixao <a64514@ualg.pt>
2023-10-14 19:17:13 +0100

    Update

--- Introductory_Problems/coin_piles.cpp
+++ Introductory_Problems/coin_piles.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
-int main () {
-	
+string pile(long r, long l) {
+	if (r == l) {
+		if (r == 0) return "YES";
+		else if (r % 2 == 0) return "NO";
+		else return "YES";
+	}
+	else {
+		if (r == 0 || l == 0) return "NO";
+		else if (r - l > 3) return "NO";
+		else return "YES";
+	}
+}
+
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		long r, l;
+		cin >> r >> l;
+		cout << pile(r, l) << endl;
+	}
 	return 0;
 }
--- Introductory_Problems/two_sets.cpp
+++ Introductory_Problems/two_sets.cpp
@@ -2,7 +2,60 @@
 
 using namespace std;
 
-int main () {
-	
+void print(int* a, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	if (n < 2) {
+		cout << "NO";
+		return 0;
+	}
+	int a[n];
+	int b[n];
+	int sum_a = 0;
+	int sum_b = 0;
+	int aa = 0;
+	int bb = 0;
+	for (int i = n; i > 1; i--) {
+		if (i % 2 == 0) {
+			a[aa++] = i;
+			sum_a += i;
+		}
+		else {
+			b[bb++] = i;
+			sum_b += i;
+		}
+	}
+	if (sum_a > sum_b) {
+		b[bb++] = 1;
+	}
+	else {
+		a[aa++] = 1;
+	}
+	if (sum_a == sum_b) {
+		if (n % 2 == 0) {
+			cout << "YES" << endl;
+			cout << sum_a << endl;
+			print(a, aa);
+			cout << endl << sum_b << endl;
+			print(b, bb);
+		}
+		else {
+			cout << "YES" << endl;
+			cout << sum_b << endl;
+			print(b, bb);
+			cout << endl << sum_a << endl;
+			print(a, aa);
+		}
+	}
+	else {
+		cout << "NO";
+	}
+
 	return 0;
 }
